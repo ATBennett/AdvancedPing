a6c5e2aa0439928a10ebcbea3ae02d292e11be99
Andrew Bennett <Andrew.Bennett@acrobat.uk.com>
2019-01-20 23:33:26 +0000

    Update - Refactored to use view model. Improved speed on console update. Reenabled copnsole history.

--- AdvancedPing/AdvancedPing/MainWindow.xaml.cs
+++ AdvancedPing/AdvancedPing/MainWindow.xaml.cs
@@ -13,32 +13,33 @@ namespace AdvancedPing
     {
         private PingManager _currentPingManager;
         private CancellationTokenSource _pingCancellationTokenSource;
-        private StringBuilder _consoleOutput = new StringBuilder();
+        private MainWindowViewModel viewModel;
 
         public MainWindow()
         {
             InitializeComponent();
             Start.IsEnabled = true;
             Stop.IsEnabled = false;
+            viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
         }
 
         private async void Start_OnClick(object sender, RoutedEventArgs e)
         {
             Start.IsEnabled = false;
 
-            var address = Address.Text;
-
-            if (!int.TryParse(Delay.Text, out var delay) ||
-                !int.TryParse(Count.Text, out var count) ||
-                string.IsNullOrWhiteSpace(address) ||
-                delay < 1 || count < 1)
+            var address = viewModel.Address;
+            if (string.IsNullOrWhiteSpace(address) ||
+                viewModel.Delay < 1 || viewModel.Count < 1)
             {
                 StatusText.Text = "Error, please check your inputs";
                 Start.IsEnabled = true;
                 return;
             }
 
-            _currentPingManager = new PingManager(count, delay, address);
+            _currentPingManager = new PingManager(viewModel.Count, viewModel.Delay, address);
+
+            //Attach OnPing event
             _currentPingManager.PingReceived += PingManager_OnPing;
             AppendOutput("Beginning ping...");
 
@@ -55,44 +56,34 @@ namespace AdvancedPing
                 AppendOutput("Cancelled!");
             }
 
+            //Remove OnPing event to make sure garbage collector works correctly
             _currentPingManager.PingReceived -= PingManager_OnPing;
+
             Stop.IsEnabled = false;
             Start.IsEnabled = true;
         }
 
         private void AppendOutput(string newLine)
         {
-            _consoleOutput.Clear();
-            _consoleOutput.AppendLine(newLine);
-            Output.Text = _consoleOutput.ToString();
+            viewModel.ConsoleOutput.Add(newLine);
             ScrollViewer.ScrollToEnd();
         }
 
         private void PingManager_OnPing(object sender, PingEventArgs args)
         {
             AppendOutput($"Reply from {args.PingReply.Address} took {args.PingReply.RoundtripTime}ms");
-            UpdateResults(args.PingResults);
-        }
-
-        private void UpdateResults(PingResults results)
-        {
-            Max.Text = results.Max.ToString();
-            Min.Text = results.Min.ToString();
-            Completed.Text = results.Count.ToString();
-            Average.Text = results.Average.ToString("0.0");
-            MaxJitter.Text = results.MaxJitter.ToString();
-            AverageJitter.Text = results.AverageJitter.ToString("0.0");
-            PacketLoss.Text = results.PacketsLost.ToString();
-            PacketLossPercent.Text = results.PacketsLostPercent.ToString("0.0");
+            viewModel.UpdateFromResults(args.PingResults);
         }
 
         private void Stop_OnClick(object sender, RoutedEventArgs e)
         {
-            if (_currentPingManager != null)
+            if (_currentPingManager == null)
             {
-                _pingCancellationTokenSource.Cancel();
-                _currentPingManager.CancelPings();
+                return;
             }
+
+            _pingCancellationTokenSource.Cancel();
+            _currentPingManager.CancelPings();
         }
 
         private void About_OnClick(object sender, RoutedEventArgs e)
---
+++ AdvancedPing/AdvancedPing/MainWindowViewModel.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AdvancedPing.Annotations;
+
+namespace AdvancedPing
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _address;
+        private ObservableCollection<string> _consoleOutput;
+        private int _count;
+        private int _delay;
+        private long _completed;
+        private double _averagePing;
+        private long _maxPing;
+        private long _minPing;
+        private long _packetsLost;
+        private double _packetsLostPercentage;
+        private long _maxJitter;
+        private double _averageJitter;
+
+        public ObservableCollection<string> ConsoleOutput
+        {
+            get => _consoleOutput;
+            set
+            {
+                if (Equals(value, _consoleOutput)) return;
+                _consoleOutput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                if (value == _address) return;
+                _address = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                if (value == _count) return;
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Delay
+        {
+            get => _delay;
+            set
+            {
+                if (value == _delay) return;
+                _delay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long Completed
+        {
+            get => _completed;
+            set
+            {
+                if (value == _completed) return;
+                _completed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double AveragePing
+        {
+            get => _averagePing;
+            set
+            {
+                if (value.Equals(_averagePing)) return;
+                _averagePing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long MaxPing
+        {
+            get => _maxPing;
+            set
+            {
+                if (value == _maxPing) return;
+                _maxPing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long MinPing
+        {
+            get => _minPing;
+            set
+            {
+                if (value == _minPing) return;
+                _minPing = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long PacketsLost
+        {
+            get => _packetsLost;
+            set
+            {
+                if (value == _packetsLost) return;
+                _packetsLost = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double PacketsLostPercentage
+        {
+            get => _packetsLostPercentage;
+            set
+            {
+                if (value.Equals(_packetsLostPercentage)) return;
+                _packetsLostPercentage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long MaxJitter
+        {
+            get => _maxJitter;
+            set
+            {
+                if (value == _maxJitter) return;
+                _maxJitter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double AverageJitter
+        {
+            get => _averageJitter;
+            set
+            {
+                if (value.Equals(_averageJitter)) return;
+                _averageJitter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Count = 100;
+            Delay = 500;
+            Address = "192.168.1.1";
+            ConsoleOutput = new ObservableCollection<string>{"Ready"};
+        }
+
+        public void UpdateFromResults(PingResults results)
+        {
+            Completed = results.Count;
+            AveragePing = results.Average;
+            MaxPing = results.Max;
+            MinPing = results.Min;
+            PacketsLost = results.PacketsLost;
+            PacketsLostPercentage = results.PacketsLostPercent;
+            MaxJitter = results.MaxJitter;
+            AverageJitter = results.AverageJitter;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
